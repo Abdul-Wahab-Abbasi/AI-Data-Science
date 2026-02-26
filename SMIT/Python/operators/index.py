998c92c15acbaa6a8034afe499aecf56b5496cf2
Abdul Wahab Abbasi <wahababbasi923@gmail.com>
2026-02-26 06:20:12 +0500

    Python Arthemetic Operators

---
+++ SMIT/Python/operators/index.py
@@ -0,0 +1,12 @@
+x = 40
+y = 25
+######################
+# ARTHEMETIC OPERATORS
+######################
+print(x + y)  # Addition
+print(x - y)  # Subtraction
+print(x * y)  # Multiplication
+print(x / y)  # Division (returns float)
+print(x % y)  # Modulus (returns reminder of division)
+print(x ** y) # Exponentiation 
+print(x // y) # Floor Division (returns int part)
